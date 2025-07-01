2ba2498b22589c2ce607ce710eab9a6744fc715a
emiduf <emiliedufseth@hotmail.com>
2025-07-01 18:24:49 +0200

    opprettet Spring Boot-prosjekt

---
+++ src/main/java/com/emiduf/avvikssystem/AvvikssystemApplication.java
@@ -0,0 +1,13 @@
+package com.emiduf.avvikssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvvikssystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvvikssystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emiduf/avvikssystem/AvvikssystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emiduf.avvikssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvvikssystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
